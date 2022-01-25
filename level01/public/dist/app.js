92b36908b3b9a946bca027eb38f954d873c6c972
joselesquinas <tecnicjoncs@gmail.com>
2022-01-25 16:09:31 +0100

    final nivel 1

--- level01/public/dist/app.js
+++ level01/public/dist/app.js
@@ -8,34 +8,47 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-document.addEventListener("DOMContentLoaded", fetchDadJoke, false);
+document.addEventListener("DOMContentLoaded", () => { fetchDadJoke(); });
 const API_URL = "https://icanhazdadjoke.com";
 const Paragraph = document.querySelector("#p-joke");
-const Button = document.getElementById("btn-new-joke");
-// reportJokes
-function fetchDadJoke() {
-    return __awaiter(this, void 0, void 0, function* () {
+const btnNew = document.getElementById("btn-new-joke");
+const btnPoc = document.getElementById("btn-poc");
+const btnRegular = document.getElementById("btn-regular");
+const btnMolt = document.getElementById("btn-molt");
+// API Rest
+const fetchDadJoke = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
         const response = yield fetch(API_URL, {
-            headers: {
-                Accept: "application/json",
-            },
+            headers: { Accept: "application/json" },
         });
         const myjoke = yield response.json();
-        Paragraph.textContent = myjoke.joke;
-    });
-}
-function listArray() {
-    let reportJokes = {};
-    reportJokes = {
-        joke: Paragraph.innerText,
-        score: 1,
-        date: new Date(Date.now()),
-    };
-    let myArray = [];
-    myArray.push(reportJokes);
-    console.log(myArray);
+        // console.log(myjoke);
+        Paragraph.textContent = `" ${myjoke.joke} "`;
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
+// score
+let newScore = 2;
+btnPoc.addEventListener("click", () => { newScore = parseInt(btnPoc.value); });
+btnRegular.addEventListener("click", () => { newScore = parseInt(btnRegular.value); });
+btnMolt.addEventListener("click", () => { newScore = parseInt(btnMolt.value); });
+btnNew.addEventListener("click", () => {
+    listArray();
     fetchDadJoke();
-}
-Button.addEventListener("click", listArray);
-//====================
+});
+//ejercicio 3 =================================================
+const jokesReports = [];
+const listArray = () => {
+    const reportJokes = {
+        joke: Paragraph.innerText,
+        score: newScore,
+        date: new Date().toISOString()
+    };
+    jokesReports.push(reportJokes);
+    newScore = 2;
+    console.log(reportJokes);
+    console.log(jokesReports);
+};
 //# sourceMappingURL=app.js.map
--- level01/src/app.ts
+++ level01/src/app.ts
@@ -1,39 +1,55 @@
-document.addEventListener("DOMContentLoaded", fetchDadJoke, false);
+document.addEventListener("DOMContentLoaded", () => { fetchDadJoke() });
 
 const API_URL: string = "https://icanhazdadjoke.com";
-const Paragraph = document.querySelector("#p-joke") as HTMLParagraphElement;
-const Button = document.getElementById("btn-new-joke") as HTMLButtonElement;
 
-interface ReportAcudits {
+const Paragraph = document.querySelector("#p-joke") as HTMLParagraphElement;
+const btnNew = document.getElementById("btn-new-joke") as HTMLButtonElement;
+const btnPoc = document.getElementById("btn-poc") as HTMLButtonElement;
+const btnRegular = document.getElementById("btn-regular") as HTMLButtonElement;
+const btnMolt = document.getElementById("btn-molt") as HTMLButtonElement;
+
+interface ReportJokes {
    joke: string;
    score: number;
-   date: Date;
+   date: string;
 }
 
-
-// reportJokes
-async function fetchDadJoke() {
-   const response = await fetch(API_URL, {
-      headers: {
-         Accept: "application/json",
-      },
-   });
-   const myjoke = await response.json();
-   Paragraph.textContent = myjoke.joke;
+// API Rest
+const fetchDadJoke = async () => {
+   try {
+      const response = await fetch(API_URL, {
+         headers: { Accept: "application/json" },
+      });
+      const myjoke = await response.json();
+      // console.log(myjoke);
+      Paragraph.textContent = `" ${myjoke.joke} "`;
+   } catch (error) {
+      console.log(error)
+   }
 }
 
-function listArray() {
-   let reportJokes = {} as ReportAcudits;
-   reportJokes = {
-      joke: Paragraph.innerText,
-      score: 1,
-      date: new Date(Date.now()),
-   };
-   let myArray = [];
-   myArray.push(reportJokes);
-   console.log(myArray);
+// score
+let newScore: number = 2;
+btnPoc.addEventListener("click", () => { newScore = parseInt(btnPoc.value) });
+btnRegular.addEventListener("click", () => { newScore = parseInt(btnRegular.value) });
+btnMolt.addEventListener("click", () => { newScore = parseInt(btnMolt.value) });
+btnNew.addEventListener("click", () => {
+   listArray();
    fetchDadJoke();
+});
+
+//ejercicio 3 =================================================
+const jokesReports: ReportJokes[] = []
+const listArray = () => {
+   const reportJokes: ReportJokes = {
+      joke: Paragraph.innerText,
+      score: newScore,
+      date: new Date().toISOString()
+   };
+   jokesReports.push(reportJokes);
+   newScore = 2;
+   console.log(reportJokes);
+   console.log(jokesReports);
+
 }
 
-Button.addEventListener("click", listArray);
-//====================
